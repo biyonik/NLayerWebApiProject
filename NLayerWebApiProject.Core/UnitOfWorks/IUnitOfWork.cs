6b70ddb9129a2c6d970f5b667bd5732af2e0a55c
Ahmet Altun <ahmet.altun60@hotmail.com>
2021-01-14 21:23:37 +0300

    IUnitOfWork arayüzü tasarlandı

---
+++ NLayerWebApiProject.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using NLayerWebApiProject.Core.Repository;
+
+namespace NLayerWebApiProject.Core.UnitOfWorks
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        IProductRepository Products { get; }
+        ICategoryRepository Categories { get;  }
+        
+        Task CommitAsync();
+        void Commit();
+    }
+}
