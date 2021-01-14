cafda4e2a32fd6dc8a0ef7254973251bd5f11ca0
Ahmet Altun <ahmet.altun60@hotmail.com>
2021-01-14 19:33:52 +0300

    ICategoryRepository ve IProductRepository arayüzleri oluşturuldu

---
+++ NLayerWebApiProject.Core/Repository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using NLayerWebApiProject.Core.Models;
+
+namespace NLayerWebApiProject.Core.Repository
+{
+    public interface ICategoryRepository: IGenericRepository<Category>
+    {
+        Task<Category> GetWithProductsByIdAsync(int categoryId);
+    }
+}
---
+++ NLayerWebApiProject.Core/Repository/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using NLayerWebApiProject.Core.Models;
+
+namespace NLayerWebApiProject.Core.Repository
+{
+    public interface IProductRepository: IGenericRepository<Product>
+    {
+        Task<Product> GetWithCategoryByIdAsync(int productId);
+    }
+}
