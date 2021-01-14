5fef09685edab8c770d3afafba1815df76753e45
Ahmet Altun <ahmet.altun60@hotmail.com>
2021-01-14 21:16:45 +0300

    Service arayüzleri implemente edildi

---
+++ NLayerWebApiProject.Core/Services/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using NLayerWebApiProject.Core.Models;
+
+namespace NLayerWebApiProject.Core.Services
+{
+    public interface ICategoryService: IService<Category>
+    {
+        Task<Category> GetWithProductsByIdAsync(int categoryId);
+    }
+}
---
+++ NLayerWebApiProject.Core/Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using NLayerWebApiProject.Core.Models;
+
+namespace NLayerWebApiProject.Core.Services
+{
+    public interface IProductService: IService<Product>
+    {
+        // bool ControlInnerBarcode(Product product);
+        Task<Product> GetWithCategoryByIdAsync(int productId);
+    }
+}
