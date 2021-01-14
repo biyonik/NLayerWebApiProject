ebfc83af5c78c76fbdb866c5071104ce55883198
Ahmet Altun <ahmet.altun60@hotmail.com>
2021-01-14 19:14:25 +0300

    Core katmanında Category ve Product entityleri oluşturuldu

---
+++ NLayerWebApiProject.Core/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace NLayerWebApiProject.Core.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
